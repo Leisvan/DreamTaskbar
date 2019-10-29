23cb4ab814d42f5a225ebaaf0fdaea190330494f
Leisvan Cordero <rathmA@LIVE.COM>
2019-10-29 01:40:24 -0400

    Creating .Models namespace for IconTitleSubtitleBundle

--- DreamedTaskBar/MainPage.xaml.cs
+++ DreamedTaskBar/MainPage.xaml.cs
@@ -68,23 +68,4 @@ namespace DreamTaskbar
             return (T)themeDictionary[resKey];
         }
     }
-
-    public class IconTitleSubtitleBundle
-    {
-        public string Glyph { get; set; }
-        public string Title { get; set; }
-        public string Subtitle { get; set; }
-        public string StatusGlyph { get; set; }
-        public string StatusText { get; set; }
-
-
-        public IconTitleSubtitleBundle(string glyph, string title, string subtitle = null, string statusGlyph = null, string statusText=null)
-        {
-            Glyph = glyph;
-            Title = title;
-            Subtitle = subtitle;
-            StatusGlyph = statusGlyph;
-            StatusText = statusText;
-        }
-    }
 }
---
+++ DreamedTaskBar/Models/IconTitleSubtitleBundle.cs
@@ -0,0 +1,23 @@
+﻿// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DreamTaskbar.Models
+{
+    public class IconTitleSubtitleBundle
+    {
+        public string Glyph { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string StatusGlyph { get; set; }
+        public string StatusText { get; set; }
+
+
+        public IconTitleSubtitleBundle(string glyph, string title, string subtitle = null, string statusGlyph = null, string statusText=null)
+        {
+            Glyph = glyph;
+            Title = title;
+            Subtitle = subtitle;
+            StatusGlyph = statusGlyph;
+            StatusText = statusText;
+        }
+    }
+}
